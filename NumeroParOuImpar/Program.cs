1189aca242f1cab8e441218ac0ba9ef66555683f
Geison Amorim <geisonam43@gmail.com>
2022-04-12 09:35:04 -0300

    Add project files.

---
+++ NumeroParOuImpar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+
+            int A;
+
+            //Entrada 
+
+            A = int.Parse(Console.ReadLine());
+
+            //Processamento
+
+            if ((A % 2) == 0)
+                Console.WriteLine("PAR");
+            else
+                Console.WriteLine("IMPAR");
+        }
+    }
+}
